93f0d0c6633ec53d431a3da8ddf86eb9555e11cc
Daniyal-Lodhi <daniyallodhi25@gmail.com>
2024-06-02 15:55:22 +0500

    fix: (Welcome Screen) replaced named routing with material page routing

--- lib/views/onBoardingScreens/welcome_screen.dart
+++ lib/views/onBoardingScreens/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trim_time/colors/custom_colors.dart';
+import 'package:trim_time/views/onBoardingScreens/get_started_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -8,43 +9,51 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: InkWell(
-        onTap: (){Navigator.pushNamed(context, '/get_started');
-},
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => GetStartedScreen()),
+          );
+        },
         child: Container(
           alignment: Alignment.bottomLeft,
-          padding: EdgeInsets.only(bottom: 40,left: 25),
+          padding: EdgeInsets.only(bottom: 40, left: 25),
           decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/images/welcome_bg.jpg'),
               fit: BoxFit.cover,
             ),
-            ),
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-        
-              children: [
-                Text('Welcome to 👋 ' ,style: TextStyle(
-                  color: CustomColors.white,
-                  fontSize: 38,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: 'rubik'
-                ),
-                ),
-                SizedBox(height: 1,),
-                Text('Trim Timer' ,style: TextStyle(
-                  color: CustomColors.peelOrange,
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'dmsans'
-                ),
-                ),
-                SizedBox(height: 10),
-                Text("The best barber & salon app in the \ncountry for your good looks and beauty." ,style: TextStyle(
-                  color: CustomColors.white
-                ),)
-              ],
-            ),
+          ),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome to 👋 ',
+                style: TextStyle(
+                    color: CustomColors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'rubik'),
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              Text(
+                'Trim Timer',
+                style: TextStyle(
+                    color: CustomColors.peelOrange,
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'dmsans'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "The best barber & salon app in the \ncountry for your good looks and beauty.",
+                style: TextStyle(color: CustomColors.white),
+              )
+            ],
+          ),
         ),
       ),
     );
